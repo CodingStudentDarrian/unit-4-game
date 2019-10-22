0848d211ec365e21c1074a70942ade828e661766
Darrian Bethea <codingstudentdarrian@gmail.com>
2019-10-22 18:23:00 -0500

    updated JS

--- assets/javaScript/javaScript.js
+++ assets/javaScript/javaScript.js
@@ -1,92 +1,102 @@
-$(document).ready(function() {
-    var Random = Math.floor(Math.random() * 101 + 19)
-        // Selects a random number to be shown at the start of the game
-        // Number should be should be between 19 - 120
-        //
-    $('#randomNumber').text(Random);
-    // Appending random number to the randomNumber id in the html doc
-    //
-    var num1 = Math.floor(Math.random() * 11 + 1)
-    var num2 = Math.floor(Math.random() * 11 + 1)
-    var num3 = Math.floor(Math.random() * 11 + 1)
-    var num4 = Math.floor(Math.random() * 11 + 1)
-        // Setting up random numbers for each jewel
-        // Random number has to be between 1 - 12
-        // 
-    var userTotal = 0;
-    var wins = 0;
-    var losses = 0;
-    //  Decaring variables for tallies
-    $('#numberWins').text(wins);
-    $('#numberLosses').text(losses);
-    //resets the game
-    function reset() {
-        Random = Math.floor(Math.random() * 101 + 19);
-        console.log(Random)
-        $('#randomNumber').text(Random);
-        num1 = Math.floor(Math.random() * 11 + 1);
-        num2 = Math.floor(Math.random() * 11 + 1);
-        num3 = Math.floor(Math.random() * 11 + 1);
-        num4 = Math.floor(Math.random() * 11 + 1);
-        userTotal = 0;
-        $('#finalTotal').text(userTotal);
-    }
-    //adds the wins to the userTotal
-    function yay() {
-        alert("You won!");
-        wins++;
-        $('#numberWins').text(wins);
-        reset();
-    }
-    //addes the losses to the userTotal
-    function loser() {
-        alert("You lose!");
-        losses++;
-        $('#numberLosses').text(losses);
-        reset()
-    }
-    //sets up click for jewels
-    $('#one').on('click', function() {
-        userTotal = userTotal + num1;
-        console.log("New userTotal= " + userTotal);
-        $('#finalTotal').text(userTotal);
-        //sets win/lose conditions
-        if (userTotal == Random) {
-            yay();
-        } else if (userTotal > Random) {
-            loser();
-        }
-    })
-    $('#two').on('click', function() {
-        userTotal = userTotal + num2;
-        console.log("New userTotal= " + userTotal);
-        $('#finalTotal').text(userTotal);
-        if (userTotal == Random) {
-            yay();
-        } else if (userTotal > Random) {
-            loser();
-        }
-    })
-    $('#three').on('click', function() {
-        userTotal = userTotal + num3;
-        console.log("New userTotal= " + userTotal);
-        $('#finalTotal').text(userTotal);
-        //sets win/lose conditions
-        if (userTotal == Random) {
-            yay();
-        } else if (userTotal > Random) {
-            loser();
-        }
-    })
-    $('#four').on('click', function() {
-        userTotal = userTotal + num4;
-        console.log("New userTotal= " + userTotal);
-        $('#finalTotal').text(userTotal);
+$(document).ready(function () {
+    let Random = Math.floor(Math.random() * 200 + 101)
 
-        if (userTotal == Random) {
-            yay();
-        } else if (userTotal > Random) {
+    $('#goal').text(Random);
+
+    let diamond = Math.floor(Math.random() * 24 + 1)
+    let ruby = Math.floor(Math.random() * 24 + 1)
+    let sapphire = Math.floor(Math.random() * 24 + 1)
+    let emerald = Math.floor(Math.random() * 24 + 1)
+
+    let Score = 0
+    let wins = 0
+    let losses = 0
+    let lives = 6
+
+    $('#Wins').text(wins);
+    $('#Losses').text(losses);
+    $('#Lives').text(lives);
+
+    $('#Diamond').on('click', function () {
+        Score = Score + diamond;
+        console.log("New Score= " + Score);
+        $('#final_Score').text(Score);
+        if (Score == Random) {
+            winner();
+        } else if (Score > Random) {
             loser();
+
         }
-    });
-});
+    })
+        / $('#Ruby').on('click', function () {
+            Score = Score + ruby;
+            console.log("New Score= " + Score);
+            $('#final_Score').text(Score);
+            if (Score == Random) {
+                winner();
+            } else if (Score > Random) {
+                loser();
+
+            }
+        })
+    $('#Sapphire').on('click', function () {
+        Score = Score + sapphire;
+        console.log("New Score= " + Score);
+        $('#final_Score').text(Score);
+        if (Score == Random) {
+            winner();
+        } else if (Score > Random) {
+            loser();
+
+        }
+    })
+    $('#Emerald').on('click', function () {
+        Score = Score + emerald;
+        console.log("New Score= " + Score);
+        $('#final_Score').text(Score);
+        if (Score == Random) {
+            winner();
+        } else if (Score > Random) {
+            loser();
+
+        }
+    })
+    function reset() {
+        Random = Math.floor(Math.random() * 200 + 101);
+        console.log(Random)
+        $('#goal').text(Random);
+        diamond = Math.floor(Math.random() * 24 + 1);
+        ruby = Math.floor(Math.random() * 24 + 1);
+        sapphire = Math.floor(Math.random() * 24 + 1);
+        emerald = Math.floor(Math.random() * 24 + 1);
+        Score = 0;
+        $('#final_Score').text(Score);
+    }
+
+    function winner() {
+        alert("Great Job!");
+        wins++;
+        $('#Wins').text(wins);
+        reset();
+
+    }
+
+    function loser() {
+        alert("Try Again");
+        losses++;
+        $('#Losses').text(losses);
+        reset();
+        dead();
+    }
+
+    function dead() {
+        if (Score != Random) {
+            lives--;
+            $('#Lives').text(lives);
+            reset();
+        } if (lives === 0) {
+            alert("Game Over, Please Try Again!");
+            location.reload();
+        }
+    }
+})
